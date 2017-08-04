8a3488dd9be2a6e0859a1c1ffb1bf7dd4a61c483
Shweta Jain <sjain@loggly.com>
2017-08-04 14:29:17 +0530

    Add map file name

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,18 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg : grunt.file.readJSON('package.json'),
-    uglify : {
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
       options: {
-        sourceMap : true
+        sourceMap: true,
+        sourceMapName: 'src/loggly.tracker-2.1.min.map'
       },
-      main : {
-        files : {
+      main: {
+        files: {
           'src/loggly.tracker-2.1.min.js' : ['src/loggly.tracker.js']
         }
       }
     }
   });
-
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', ['uglify']);
-
 };
